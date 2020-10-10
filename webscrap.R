107f8fcd1d3b54db83077d7ae2f799193b9c742d
vgyaan <40377337+vgyaan@users.noreply.github.com>
2020-10-10 21:34:59 +0530

    Update webscrap.R

--- webscrap.R
+++ webscrap.R
@@ -1,25 +1,22 @@
-# Install the Package rvest
+## Install & Evoke packages
 install.packages("rvest")
-## Evoke packages
 library(rvest)
 
-# Load the website to scrap data
-dataset <- read_html("https://www.alexa.com/topsites/countries/IN")
+# Loading websites to scrap data
+url <- read_html("https://www.alexa.com/topsites/countries/IN")
 
-# Now Select the node by using Selector Gadget or inspect option
-Sites <- dataset %>% html_nodes("p a") %>% html_text()
-Dailytime <- dataset %>% html_nodes(".DescriptionCell+ .right p") %>% html_text()
-DailyPageViews <-  dataset %>% html_nodes(".right:nth-child(4) p") %>% html_text()
-Trafficpersearch <- dataset %>% html_nodes(".right:nth-child(5) p") %>% html_text()
-Sitelinked <- dataset %>% html_nodes(".right:nth-child(6) p") %>% html_text()
 
-# Creata a DataFrame from Scraped Data
+Sites <- url %>% html_nodes(".DescriptionCell a") %>% html_text()
+Dailytime <- url %>% html_nodes(".DescriptionCell+ .right p") %>% html_text()
+DailyPageViews <-  url %>% html_nodes(".td:nth-child(4) p") %>% html_text()
+Trafficpersearch <- url %>% html_nodes(".td:nth-child(5) p") %>% html_text()
+Sitelinked <- url %>% html_nodes(".td:nth-child(6) p") %>% html_text()
+
+
+# Creating a DataFrame
 Alexatopsites <- data.frame(Sites, Dailytime, DailyPageViews, Trafficpersearch,Sitelinked)
 View(Alexatopsites)
 
 # Write the data to a file
 getwd()
 write.csv(Alexatopsites, file = "Alexa Top Sites.csv")
-
-
-
